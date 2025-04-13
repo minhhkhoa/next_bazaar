f0d9758928e843e13e52ffbc736fcd26feaf75a2
Khoa <khoanguyen.1881@gmail.com>
2025-04-13 16:23:19 +0700

    customize css input.search antd
    mở devtool để lấy tên class.
    Sử dụng scss + BEM để css.

--- src/components/Search/SearchProduct.tsx
+++ src/components/Search/SearchProduct.tsx
@@ -3,21 +3,23 @@
 import React from "react";
 import { Input } from "antd";
 import type { GetProps } from "antd";
+import "./style.scss";
 
 type SearchProps = GetProps<typeof Input.Search>;
-
 const { Search } = Input;
 
 export default function SearchProduct() {
   const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
     console.log(info?.source, value);
+
   return (
-    <div className="!w-1/3 SearchProduct">
+    <div className="!w-1/3 searchProduct">
       <Search
         placeholder="input search text"
         onSearch={onSearch}
-        enterButton
         size="large"
+        className="searchProduct"
+        enterButton
       />
     </div>
   );
