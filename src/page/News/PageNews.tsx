b1d173cc1df3641dc78a71467ee05373813614a7
Khoa <khoanguyen.1881@gmail.com>
2025-04-18 16:16:16 +0700

    success reponsive PageTinTuc
    Page này tạm chấm dứt tại đây, mặc dù code reponsive hơi lằng nhằng nhưng nó chạy ngon:)).

--- src/page/News/PageNews.tsx
+++ src/page/News/PageNews.tsx
@@ -1,18 +1,51 @@
+"use client";
+
 import { News, NewsCategory } from "@/data/news/new";
 import { NewsType } from "@/dataType/new";
-import { ArrowRightOutlined, ClockCircleOutlined, UserOutlined } from "@ant-design/icons";
+import useWindowSize from "@/hook/WindowSize/useWindowSize";
+import {
+  ArrowRightOutlined,
+  ClockCircleOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { Col, Divider, Menu, Row } from "antd";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function PageNews() {
+  const { width } = useWindowSize();
   return (
     <>
-      <div className="flex gap-5 px-32 py-5">
+      {(width ?? 0) <= 1024 && (
+        <div className="flex justify-center w-full md:px-32 py-5 text-[clamp(12px,5vw,24px)]">
+          <Col className={`bg-[#F3F3F3] rounded-xl p-2 w-full`}>
+            <h1 className=" font-bold text-center">Danh mục tin tức</h1>
+            <Menu
+              // onClick={onClick}
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                width: "100%",
+              }}
+              mode="inline"
+              items={NewsCategory}
+            />
+          </Col>
+        </div>
+      )}
+      <div className="flex gap-5 md:px-32 py-5">
         {/* News category */}
-        <Row className="flex-1 gap-5 !flex-col">
-          <Col className="bg-[#F3F3F3] rounded-xl p-2">
+        <Row
+          className={`flex-1 gap-5 !flex-col  ${
+            (width ?? 0) <= 768 && "!hidden"
+          }`}
+        >
+          <Col
+            className={`bg-[#F3F3F3] rounded-xl p-2 ${
+              (width ?? 0) <= 1024 && "hidden"
+            }`}
+          >
             <h1 className="text-2xl font-bold text-center">Danh mục tin tức</h1>
             <Menu
               // onClick={onClick}
@@ -21,13 +54,17 @@ export default function PageNews() {
               items={NewsCategory}
             />
           </Col>
-          <Col className="bg-[#F3F3F3] rounded-xl pb-5">
+          <Col className={`bg-[#F3F3F3] rounded-xl pb-5`}>
             <h1 className="text-2xl font-bold text-center p-2">
               Tin tức nổi bật
             </h1>
             {News.filter((item) => item.featured).map((item, index: number) => (
               <div key={item.id}>
-                <div className="flex items-center gap-5 bg-[#F3F3F3] cursor-pointer px-5">
+                <div
+                  className={`flex items-center gap-5 bg-[#F3F3F3] cursor-pointer px-5 ${
+                    (width ?? 0) <= 1024 && "flex-col"
+                  }`}
+                >
                   <Image
                     src={item.url}
                     alt={item.title}
@@ -74,12 +111,12 @@ export default function PageNews() {
 
                     <div className="flex gap-3 italic font-medium">
                       <div>
-                        <UserOutlined className="px-2"/>
+                        <UserOutlined className="px-2" />
                         {item.author}
                       </div>
                       <>|</>
                       <div>
-                        <ClockCircleOutlined className="px-2"/>
+                        <ClockCircleOutlined className="px-2" />
                         {item.date_publish}
                       </div>
                     </div>
@@ -87,7 +124,8 @@ export default function PageNews() {
                     <div className="line-clamp-2">{content[0].description}</div>
 
                     <Link href={`/news/${item.id}`}>
-                      Xem chi tiết <ArrowRightOutlined style={{ color: "#fe9614" }}/>
+                      Xem chi tiết{" "}
+                      <ArrowRightOutlined style={{ color: "#fe9614" }} />
                     </Link>
                   </div>
                 </Col>
